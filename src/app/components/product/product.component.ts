7dc1b94cac41bc007af1a783bcf81198abebb8ed
Mohamed-Awaad <awaad66642@gmail.com>
2022-11-03 21:22:12 +0200

    create ts file fot Products

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { IProduct } from 'src/app/models/iproduct';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnChanges {
+  productList: IProduct[] = [];
+  date: Date;
 
-  constructor() { }
+  orderTotalPrice: number = 0;
+  @Input() selectedCategoryId: number = 0;
+  @Output() totalPriceChanged: EventEmitter<number>;
 
-  ngOnInit(): void {
+  constructor(private prodService: ProductsService, private router: Router) {
+    this.date = new Date();
+
+    this.totalPriceChanged = new EventEmitter<number>();
   }
 
+  ngOnChanges(): void {
+    this.productList = this.prodService.getProductsByCategoryID(this.selectedCategoryId);
+  }
+
+  ngOnInit(): void {
+    this.productList = this.prodService.getAllProducts();
+  }
+
+  trackById(index: number, prod: IProduct): number {
+    return prod.id;
+  }
+
+  buy(price: number, count: number): void {
+    this.orderTotalPrice += count * price;
+
+    this.totalPriceChanged.emit(this.orderTotalPrice);
+  }
+
+  openProductDetails(id: number): void {
+    this.router.navigateByUrl(`products/${id}`);
+  }
 }
